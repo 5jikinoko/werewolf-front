c903fceae9213fd679e513801939d444ad2dc995
5jikinoko <66122830+5jikinoko@users.noreply.github.com>
2021-07-10 22:35:49 +0900

    Update index.js

--- search-room/src/index.js
+++ search-room/src/index.js
@@ -1,27 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
+import ProfileModal from "./Profile-modal";
 
+class RoomSearch extends React.Component {
 
-function RoomSearch () {
-
-  const constructor = (props) => {
-    super(props);
+  constructor(props) {
+    super(props)
     this.state = {
       error: null,
       isLoaded: false,
-      a: []
+      roomList: {}
     };
   }
 
-  const componentDidMount = () => {
-    fetch("https://jsondata.okiba.me/v1/json/jjzmI210610024513")
+  componentDidMount() {
+    fetch("http://160.16.141.77:51000/room-list")
       .then(res => res.json())
       .then(
         (result) => {
           this.setState({
             isLoaded: true,
-            items: result.items
+            roomList: result
           });
         },
         (error) => {
@@ -34,14 +34,17 @@ function RoomSearch () {
   }
 
   //作成ボタンのクリック時動作
-  const hundleClickMake = () => {
-    location.href = "URL";
+  hundleClickMake() {
+    window.location.href = "http://160.16.141.77:51000/make-room";
   }
 
 
 
+  render(){
   //L69：表示したい情報を適宜追加
-    const { error, isLoaded, items } = this.state;
+    const error = this.state.error;
+    const isLoaded = this.state.isLoaded;
+    const roomList = this.state.roomList;
 
     if (error) {
       return <div>Error: {error.message}</div>;
@@ -49,26 +52,30 @@ function RoomSearch () {
       return <div>Loading...</div>;
     } else {
       return (
+        <div>
+        <ProfileModal/>
         <div className="container">
           <div className="center-area">
             <div className="out-flame">
-              <button className="roommake" onClick ={hundleClickMake() } type="button">部屋を作る</button>
+              <button className="roommake" onClick ={() => {this.hundleClickMake()} } type="button">部屋を作る</button>
               <div className="in-flame">
                 <div className="stute">
                   部屋一覧
                 </div>
                 <ul className="UL">              
-                  {items.map(item => (
-                    <li key={item.id} className="box3" type="button">
-                      {item.name} {item.price}
-                      <p></p>
-                      {item.name} {item.price}
+                  {roomList.map(room => (
+                    <li key={room.roomID} className="box3" type="button" onClick = {() => {window.location.href="http://160.16.141.77:51000/enter-room?roomID=" + room.roomID}}>
+                      <p>{room.roomName}　部屋主：{room.hostName}</p> 
+                      <p>参加者/人数制限：{room.nowMember}/{room.maxMember}</p>
+                      <p>あいことば{room.existPass ? "あり" : "なし"}</p>
+                      <p>{room.introduction}</p>
                     </li>
                   ))}
                 </ul>
               </div>
             </div>
           </div>
+        </div>
         </div>
       );
     }
